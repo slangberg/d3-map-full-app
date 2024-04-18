8c4eefb56a1dfb020240d11c24ca23dd05a5dd7f
slangberg <samuel.langberg@gmail.com>
2024-04-17 22:53:15 -0400

    cleanu up

--- api/src/connection.js
+++ api/src/connection.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const connection = "mongodb://mongo:27017/mongo-test";
-
-const connectDb = () => {
-  return mongoose.connect(connection);
+const connectDb = async () => {
+  const connectWithRetry = () => {
+    return mongoose
+      .connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log("Database connected"))
+      .catch((err) => {
+        console.log("Database connection failed, retrying in 5 seconds");
+        setTimeout(connectWithRetry, 5000);
+      });
+  };
+  return connectWithRetry();
 };
-
 module.exports = connectDb;
