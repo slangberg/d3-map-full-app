d95a584d86ee08cd658db557f5c7b17de878893f
slangberg <samuel.langberg@gmail.com>
2024-05-14 15:18:15 -0400

    improvments

--- api/src/connection.js
+++ api/src/connection.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 // Retrieve credentials and database details from environment variables
 const username = encodeURIComponent(process.env.MONGO_INITDB_ROOT_USERNAME);
 const password = encodeURIComponent(process.env.MONGO_INITDB_ROOT_PASSWORD);
-const dbName = process.env.MONGO_DB_NAME; // Name of the database
+const dbName = encodeURIComponent(process.env.MONGO_DB_NAME); // Name of the database
 
 // Construct the MongoDB connection string with authentication details
 const connection = `mongodb://${username}:${password}@mongo:27017/${dbName}?authSource=admin`;
-
+console.log({ username, password, dbName });
 const maxAttempts = 3;
 let currentAttempts = 0;
 const connectDb = async () => {
