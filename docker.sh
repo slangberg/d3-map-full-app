00c6f85f0757784c165e6a7750e6264743b97097
slangberg <samuel.langberg@gmail.com>
2024-05-15 11:35:20 -0400

    untrack sh file

--- docker.sh
+++
@@ -1,7 +0,0 @@
-docker-compose -f docker-compose.prod.yml build
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992382616033.dkr.ecr.us-east-1.amazonaws.com
-docker tag vite-mamp-js-api:latest 992382616033.dkr.ecr.us-east-1.amazonaws.com/datalous-api:latest
-docker push 992382616033.dkr.ecr.us-east-1.amazonaws.com/datalous-api:latest
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992382616033.dkr.ecr.us-east-1.amazonaws.com
-docker tag vite-mamp-js-client:latest 992382616033.dkr.ecr.us-east-1.amazonaws.com/datalous-client:latest
-docker push 992382616033.dkr.ecr.us-east-1.amazonaws.com/datalous-client:latest
